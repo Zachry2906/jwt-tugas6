90846235d444f45b7eb8837fb2f3bab393f5c5a5
Arya <aryazakaria67@gmail.com>
2025-05-05 14:28:33 +0700

    rhrhhr

---
+++ models/UserModel.js
@@ -0,0 +1,18 @@
+import { Sequelize } from "sequelize";
+import db from "../config/Database.js";
+
+// Membuat tabel "user"
+const User = db.define(
+  "user", // Nama Tabel
+  {
+    password: Sequelize.STRING,
+    refresh_token : Sequelize.TEXT,
+    name: Sequelize.STRING,
+    email: Sequelize.STRING,
+    gender: Sequelize.STRING,
+  }
+);
+
+db.sync().then(() => console.log("Database synced"));
+
+export default User;
